a6de5ab147768586b9fd6c8fe3ff4084dc7693eb
ZeframLou <zeframlou@gmail.com>
2022-08-15 23:41:35 -0700

    fix: event.address

--- src/pair.ts
+++ src/pair.ts
@@ -2,7 +2,7 @@ import { AssetRecipientChange, DeltaUpdate, FeeUpdate, OwnershipTransferred, Spo
 import { Pair, PairOwner } from "../generated/schema"
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {
-    let pair = Pair.load(event.transaction.from.toHex())
+    let pair = Pair.load(event.address.toHex())
 
     let pairOwner = PairOwner.load(event.params.newOwner.toHex())
     if (pairOwner == null) {
@@ -16,25 +16,25 @@ export function handleOwnershipTransferred(event: OwnershipTransferred): void {
 }
 
 export function handleAssetRecipientChange(event: AssetRecipientChange): void {
-    let pair = Pair.load(event.transaction.from.toHex())
+    let pair = Pair.load(event.address.toHex())
     pair!.assetRecipient = event.params.a.toHex()
     pair!.save()
 }
 
 export function handleDeltaUpdate(event: DeltaUpdate): void {
-    let pair = Pair.load(event.transaction.from.toHex())
+    let pair = Pair.load(event.address.toHex())
     pair!.delta = event.params.newDelta
     pair!.save()
 }
 
 export function handleFeeUpdate(event: FeeUpdate): void {
-    let pair = Pair.load(event.transaction.from.toHex())
+    let pair = Pair.load(event.address.toHex())
     pair!.fee = event.params.newFee
     pair!.save()
 }
 
 export function handleSpotPriceUpdate(event: SpotPriceUpdate): void {
-    let pair = Pair.load(event.transaction.from.toHex())
+    let pair = Pair.load(event.address.toHex())
     pair!.spotPrice = event.params.newSpotPrice
     pair!.save()
 }
